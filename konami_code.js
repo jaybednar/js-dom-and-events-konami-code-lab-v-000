5866c5b72c11d51e6a8a1ee2ce294c025869c1b3
MyMac <mymac@JCB.local>
2018-05-29 08:47:59 -0700

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,26 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
 
-}
+
+function init() {
+let i = 0;
+	document.body.addEventListener('keydown', function(e){
+		const key = parseInt(e.detail || e.which);
+	  
+	  if (key === code[i]) {
+
+	  	i++;
+
+	  	if (i === code.length) {
+	  		alert("Congrats!");
+
+	  		i = 0;
+	  	}
+
+	  } else {
+	  	i = 0;
+	  }
+	});
+  
+}; 
+  // Write your JavaScript code inside the init() function
